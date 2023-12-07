fe543b6e2287d6af06ee2f0c661e63c46794c3fe
abuindario <darioabuinpose@gmail.com>
2023-12-07 10:35:56 +0100

    Modified LWC name COntentTitle --> ContentTile

--- force-app/main/default/lwc/contentGalery/contentGalery.js
+++ force-app/main/default/lwc/contentGalery/contentGalery.js
@@ -37,14 +37,3 @@ export default class ContentGalery extends NavigationMixin(LightningElement) {
     }
 
 }
-
-// Crear un LWC contentGalery que use el método getContenidoPorCliente para obtener el
-// contenido disponible para una cuenta y lo muestre usando el componente contentTile,
-// mostrando una galería de carteles, con 4 por fila. Nota: La composición de la galería (4
-// columnas de carteles) se construirá usando el component ‘lightning-layout’.
-// • Cuando un cartel sea seleccionado, el componente contentTile debe enviar la Id de dicho
-// contenido al componente contentGalery. El componente contentGalery debe abrir la página
-// del registro en una pestaña nueva.
-// • El componente contentGalery se añadirá a la Lightning Record Page de Cuenta.
-// Nota: Cuando un componente es añadido a una record page, se obtendrá dinámicamente el Id
-// del registro en el que estamos creando un atributo @api recordId en el componente.
